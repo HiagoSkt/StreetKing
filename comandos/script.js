3ff45fcf397ab6f4ee074f9053a506c78304cab4
Hiago SK <hiago.e.a22@gmail.com>
2024-04-04 15:23:42 -0300

    add  produtos

--- comandos/script.js
+++ comandos/script.js
@@ -3,3 +3,30 @@ const chk = document.getElementById('chk')
 chk .addEventListener('change', () => {
     document.body.classList.toggle('dark')
 })
+
+const carousel = document.getElementById('carousel');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 3; // Ajuste a sensibilidade de rolagem aqui
+  carousel.scrollLeft = scrollLeft - walk;
+});
